65ff1844006b3169a405174e8ad1ebc0eb523eb0
prashansa358 <prashansasinha121@gmail.com>
2022-10-26 20:34:10 +0530

    Added EventEmitter for ratingClicked

--- Keep/src/app/shared/star.component.ts
+++ Keep/src/app/shared/star.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges } from "@angular/core";
+
+import { Component, Input, OnChanges, Output,EventEmitter } from "@angular/core";
+
 
 @Component({
   selector:'app-star',
@@ -7,10 +9,17 @@ import { Component, Input, OnChanges } from "@angular/core";
 })
 
 export class StarComponent implements OnChanges{
-  @Input() rating: number = 0 ;
+  @Input()
+  rating: number = 0;
   cropWidth: number = 65;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
 
   ngOnChanges(): void {
     this.cropWidth = this.rating*65/5;
+  }
+
+  onClick():void{
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
+
   }
 }
